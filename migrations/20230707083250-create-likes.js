8a3b814ea5d07e12f1a645b9cbc317f99337552f
Juseung Kim <95juseung@gmail.com>
2023-07-07 19:55:01 +0900

    Likes 테이블 수정

--- migrations/20230707083250-create-likes.js
+++ migrations/20230707083250-create-likes.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Likes', {
-      likesId: {
+      likeId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
--- models/likes.js
+++ models/likes.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   Likes.init(
     {
-      likesId: {
+      likeId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
